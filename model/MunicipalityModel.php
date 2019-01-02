07608a5e5caf94848c9de776112f468ce902e62f
Andrés <andresrueda1996@gmail.com>
2019-01-02 11:42:30 +0100

    Corrige falta en MunicipalityModel

---
+++ model/MunicipalityModel.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "core/AbstractModel.php";
+require_once "model/dao/MunicipalityDao.php";
+
+use core\AbstractModel;
+
+class Municipality extends AbstractModel {
+
+    private $municipalityDao;
+
+    public function __construct() {
+        $this->municipalityDao = new MunicipalityDao();
+        parent::__construct($this->municipalityDao);
+    }
+
+}
