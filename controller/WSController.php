1f1c2c75455e01663d9e8744126ecac16124ceaa
Quini <quiniroiz@gmail.com>
2019-01-14 16:53:26 +0100

    añadidos content-type a los WS

--- controller/WSController.php
+++ controller/WSController.php
@@ -28,6 +28,7 @@ class WSController extends AbstractController {
         if (filter_has_var(INPUT_POST, "str") && trim($_POST['str']) != "") {
             $str = filter_var($_POST['str'], FILTER_SANITIZE_STRING);
             $list = $this->adModel->globalSearch($str);
+            header('Content-type: application/json');
             echo json_encode($list);
         } else {
             $this->redirect();
@@ -38,6 +39,7 @@ class WSController extends AbstractController {
         if (filter_has_var(INPUT_POST, "userPag") && trim($_POST['userPag']) != "") {
             $num = filter_var($_POST['userPag'], FILTER_SANITIZE_NUMBER_INT);
             $allusers = $this->userModel->getAllPaginated($num);
+            header('Content-type: application/json');
             echo json_encode($allusers);
         } else {
             $this->redirect();
@@ -48,6 +50,7 @@ class WSController extends AbstractController {
         if (filter_has_var(INPUT_POST, "adPag")) {
             $num = filter_var($_POST['adPag'], FILTER_SANITIZE_NUMBER_INT);
             $allAds = $this->adModel->getAllPaginated($num);
+            header('Content-type: application/json');
             echo json_encode($allAds);
         } else {
             $this->redirect();
@@ -58,6 +61,7 @@ class WSController extends AbstractController {
         if (filter_has_var(INPUT_POST, "communityId")) {
             $communityId = filter_var($_POST['communityId'], FILTER_SANITIZE_NUMBER_INT);
             $allProvinces = $this->provinceModel->search('community_id', $communityId);
+            header('Content-type: application/json');
             echo json_encode($allProvinces);
         } else {
             $this->redirect();
@@ -68,6 +72,7 @@ class WSController extends AbstractController {
         if (filter_has_var(INPUT_POST, "provinceId")) {
             $provinceId = filter_var($_POST['provinceId'], FILTER_SANITIZE_NUMBER_INT);
             $allProvinces = $this->municipalityModel->search('province_id', $provinceId);
+            header('Content-type: application/json');
             echo json_encode($allProvinces);
         } else {
             $this->redirect();
@@ -78,6 +83,7 @@ class WSController extends AbstractController {
         if (filter_has_var(INPUT_POST, "commentPag")) {
             $num = filter_var($_POST['commentPag'], FILTER_SANITIZE_NUMBER_INT);
             $allComments = $this->commentModel->getAllPaginated($num);
+            header('Content-type: application/json');
             echo json_encode($allComments);
         } else {
             $this->redirect();
@@ -88,6 +94,7 @@ class WSController extends AbstractController {
         if (filter_has_var(INPUT_POST, "housingTypePag")) {
             $num = filter_var($_POST['housingTypePag'], FILTER_SANITIZE_NUMBER_INT);
             $allHousingTypes = $this->housingTypeModel->getAllPaginated($num);
+            header('Content-type: application/json');
             echo json_encode($allHousingTypes);
         } else {
             $this->redirect();
@@ -98,6 +105,7 @@ class WSController extends AbstractController {
         if (filter_has_var(INPUT_POST, "operationTypePag")) {
             $num = filter_var($_POST['operationTypePag'], FILTER_SANITIZE_NUMBER_INT);
             $allOperationTypes = $this->operationTypeModel->getAllPaginated($num);
+            header('Content-type: application/json');
             echo json_encode($allOperationTypes);
         } else {
             $this->redirect();
