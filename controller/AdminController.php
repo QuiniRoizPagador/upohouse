a4bf1dadfdc02b32c76f2296e895c1b0b6e08216
Andrés <andresrueda1996@gmail.com>
2019-01-29 18:50:34 +0100

    Corrige controller/AdminController.php

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -26,6 +26,7 @@ class AdminController extends AbstractController {
     private $reportModel;
     private $operationTypeModel;
     private $requestModel;
+    private $adModel;
 
     /*
      * Constructor por defecto
